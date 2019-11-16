3f50a19bfe944e973858f48082d05d59e66e6cef
Chris Vincent <cdvincent89@gmail.com>
2019-11-16 00:45:55 -0500

    portfolio

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+$("#aboutBtn").on("click", function(){
+    $("html, body").animate({
+        scrollTop: $("#aboutScroll").offset().top
+    }, 1000);
+});
+
+$("#portfolioBtn").on("click", function(){
+    $("html, body").animate({
+        scrollTop: $("#portfolioScroll").offset().top
+    }, 1000);
+});
+
+$("#contactBtn").on("click", function(){
+    $("html, body").animate({
+        scrollTop: $("#contactScroll").offset().top
+    }, 1000);
+});
